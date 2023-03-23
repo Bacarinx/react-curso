7457fb7a7c989ac52ab8d528153e60da48feee4a
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-23 16:17:44 -0300

    comentários em JSX

--- 2-FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2-FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -1,6 +1,9 @@
 const FirstComponent = () => {
+    //essa função faz isso: 
     return (
+        
         <div>
+            {/* Algum comentário*/}
             <h1>Meu primeiro componente</h1>
             <p className= "teste"></p>
         </div>
@@ -9,7 +12,7 @@ const FirstComponent = () => {
 
 export default FirstComponent;
 
-//conclusões de 1° aula: 
+//Resumo aula 1 - EXPORTAÇÃO
 
 // 1 - Criamos uma pasta components dentro do src para colocar todos os documentos nela
 
@@ -17,7 +20,7 @@ export default FirstComponent;
 
 //----------------------------------------------------------//
 
-//Resumo aula 2 :
+//Resumo aula 2 - IMPORTAÇÂO
 
 // 1 - Para importar um arquivo utilizamos a sintaxe: import [nome Arquivo] from [lugar de exportação]
 
@@ -25,9 +28,14 @@ export default FirstComponent;
 
 //----------------------------------------------------------//
 
-//Resumo aula 3: 
+//Resumo aula 3 - JSX
 
 // 1 - Algumas tags vão ser alteradas de nome para não interferir no proprio html ou javascript, logo a tag "Name" ira ser chamada de "className"
 
 // 2 - O JSX pode ter apenas UM ELEMENTO PAI, logo todo a função será envelopada por apenas uma div
 
+//----------------------------------------------------------//
+
+//resumo aula 4 - COMENTÁRIOS NO JSX
+
+// 1 - Para realizar um comentário dentro da função no JSX, utilizamos a seguinte sintaxe: { /* texto */ }
