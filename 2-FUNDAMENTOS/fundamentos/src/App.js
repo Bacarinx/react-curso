d5ca18664e17e2d0a300ebb3580825bed6f1c98e
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-23 16:02:22 -0300

    importação

--- 2-FUNDAMENTOS/fundamentos/src/App.js
+++ 2-FUNDAMENTOS/fundamentos/src/App.js
@@ -1,11 +1,22 @@
+//Components
+import FirstComponent from './components/FirstComponent';
+
+//styles /CSS
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Fundamentos React</h1>
+      <FirstComponent />
     </div>
   );
-}
+};
 
 export default App;
+
+//Resumo aula 2 :
+
+//Para importar um arquivo utilizamos a sintaxe: import [nome Arquivo] from [lugar de exportação]
+
+//para usar o arquivo importado utilizamos asintaxe: <NomeCOmponente />
--- 2-FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2-FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -11,4 +11,5 @@ export default FirstComponent;
 //conclusões de 1° aula: 
 
 // 1 - Criamos uma pasta components dentro do src para colocar todos os documentos nela
+
 // 2 - criamos uma função que será exportada para outro arquivo, iniciado por uma arrow function
