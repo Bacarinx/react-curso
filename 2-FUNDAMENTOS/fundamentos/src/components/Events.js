ac52b248b4b21085386b13e13f94f1a052eb7372
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-23 17:00:02 -0300

    Update Events.js

--- 2-FUNDAMENTOS/fundamentos/src/components/Events.js
+++ 2-FUNDAMENTOS/fundamentos/src/components/Events.js
@@ -9,6 +9,14 @@ const Events = () => {
             <div>
                 <button onClick={handleMyEvent}>CLique aqui</button>
             </div>
+            <div>
+                <button onClick={() => console.log("Clicou")}>Clique Aqui também</button>
+                <button onClick={() => {
+                    if(true) {
+                        console.log("isso não deveria existir")
+                    }
+                }}>Clique aqui, por favor</button>
+            </div>
         </div>
     )
 };
