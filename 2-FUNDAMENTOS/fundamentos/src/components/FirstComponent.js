1a17cad3bb342e931ec7af880a3e33b1bec2f180
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-23 15:55:21 -0300

    criando componente

---
+++ 2-FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -0,0 +1,14 @@
+const FirstComponent = () => {
+    return (
+        <div>
+            <h1>Meu primeiro componente</h1>
+        </div>
+    );
+};
+
+export default FirstComponent;
+
+//conclusões de 1° aula: 
+
+// 1 - Criamos uma pasta components dentro do src para colocar todos os documentos nela
+// 2 - criamos uma função que será exportada para outro arquivo, iniciado por uma arrow function
