566d6577a16f9b35006099e5a2d632fa430faa5e
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-23 21:31:34 -0300

    img public

---
+++ 3-AVANCANDO/avancando/src/App.js
@@ -0,0 +1,21 @@
+//import CSS
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Avançando em React</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="/img1.jpg" alt="Paisagem"/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
+/*resumo aula 1 - imagens na pasta public
+1 - Para usar uma imagem da pasta public, basta utilizarmos a tag img, e no src colocar "/" e em seguida o nome da imagem Ex: /img1.jpg
+*/
+
