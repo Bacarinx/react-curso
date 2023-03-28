3f67fef98421a858d6fe5fe72d5dbb085fa19a5c
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-28 14:56:16 -0300

    Css de componente

--- 4_CSS_REACT/css/src/App.js
+++ 4_CSS_REACT/css/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import MyComponent from './components/MyComponent';
 
 function App() {
   return (
     <div className="App">
-    
+      {/* CSS Global */}
+      <h1>React com Css</h1>
+      {/* Css de componente */}
+      <MyComponent />
+      <p>Este paragrafo é do App.js</p>
     </div>
   );
 }
