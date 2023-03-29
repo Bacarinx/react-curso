becbd1015acd84527eb5507b6c54737e83a242a1
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-29 16:17:49 -0300

    formulário em react

--- 5_FORM_EM_REACT/form/src/App.js
+++ 5_FORM_EM_REACT/form/src/App.js
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
         <h2>Forms</h2>
-        < MyForm />
+        <MyForm user={{
+        name: "Josias",
+        email:"Josias@email.com",
+        bio:"Sou um advogado",
+        role: "editor"
+        }}/>
     </div>
   );
 }
--- 5_FORM_EM_REACT/form/src/components/MyForm.js
+++ 5_FORM_EM_REACT/form/src/components/MyForm.js
@@ -1,13 +1,63 @@
 import './MyForm.css'
 
-const MyForm = () => {
+import {useState} from "react"
+
+const MyForm = ({user}) => {
+  //6 - Controlled Input
+  //3 - Gerenciamento de dados 
+  const [name, setName] = useState(user ? user.name : '')
+  const [email, setEmail] = useState(user ? user.email : '')
+  const [bio, setBio] = useState(user? user.bio : '')
+  const [role, setRole] = useState(user ? user.role : '')
+
+
+  const handleName = (e) => {
+    setName(e.target.value)
+  }
+
+  console.log(name)
+  console.log(email)
+  console.log(bio)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("enviando o form")
+    console.log(name, email, bio, role)
+
+    // 7 - limpar forms
+    setName("")
+    setEmail("")
+    setBio("")
+  }
+
   return (
     <div>
-        {/* Criação de Form */}
-        <form>
+        {/*5- Envio de Form */}
+        {/*1- Criação de Form */}
+        <form onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Nome</label>
-                <input type="text" name="name" placeholder="Digíte o seu nome"/>
+                <input type="text" name="name" placeholder="Digíte o seu nome" onChange={handleName} value={name}/>
+                {/* 2- Label envolvendo Input */}
+            <label>
+              {/* 4- Simplificação de manipulação de state */}
+              <span>E-mail:</span>
+              <input type="email" name="email" placeholder="Digíte sei email" onChange={(e) => setEmail(e.target.value)} value={email}/>
+            </label>
+            {/* 8 - textArea */}
+            <label>
+              <span>BIO:</span>
+              <textarea name="" placeholder='Descrição do usuário' onChange={(e) => setBio(e.target.value)} value={bio}></textarea>
+            </label>
+            {/* 9- select */}
+            <label>
+              <span>função no sistema:</span>
+              <select name="role" onChange={(e) => setRole(e.target.value)} value={role}>
+                <option value="user">Usuário</option>
+                <option value="editor">Editor</option>
+                <option value="admin">Adiministrador</option>
+              </select>
+            </label>
             </div>
             <input type="submit" value="enviar"/>
         </form>
