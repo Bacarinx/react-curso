2d167bffabdf4bf543435ff1e0c7f1a69ae8679f
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-03-30 21:43:52 -0300

    Escolhendo a palavra aleatória

--- 6_SECRET_WORD/word/src/App.js
+++ 6_SECRET_WORD/word/src/App.js
@@ -19,13 +19,52 @@ const stages = [
   {id: 3, name: "end"}
 ]
 
+  
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name) //criando a variável para mudar para cada etapa do game
 
   const [words] = useState(wordsList) //setando as palavras do jogo em um hook useState
 
+  const [pickedWord, setPickedWord] = useState("")
+  const [pickedCategory, setPickedCategory] = useState("")
+  const [letters, setLetters] = useState([])
+
+  const pickWordAndCategory = () => {
+    //pick a random category
+    const categories = Object.keys(words)
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)]
+    console.log(category)
+
+    //pick a random Word
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+    console.log(word)
+
+    return {word, category}
+
+  }
+
   //starts the secret word Game
   const startGame = () => { 
+    //pick Word and pick category
+    const {word, category} = pickWordAndCategory()
+
+    //create an array of letters
+    let wordLetters = word.split("")
+    
+    //Deixando todas as letras em lower Case
+    wordLetters = wordLetters.map((l) => {
+      return l.toLowerCase()
+    })
+
+    console.log(word, category)
+    console.log(wordLetters)
+
+    //fill states
+    setPickedWord(word)
+    setPickedCategory(category)
+    setLetters(letters)
+
     setGameStage(stages[1].name)
   }
 
