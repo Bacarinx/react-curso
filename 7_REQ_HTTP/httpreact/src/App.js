39bb8b6219b13177b8424ecf7602abe24f46f8b0
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-04-04 17:43:29 -0300

    adicionando infos sobre o useEffect

--- 7_REQ_HTTP/httpreact/src/App.js
+++ 7_REQ_HTTP/httpreact/src/App.js
@@ -9,3 +9,14 @@ function App() {
 }
 
 export default App;
+
+/* UseEffect hook
+
+1- Ele faz com que determinada ação seja executada somente uma vez
+
+2 - Impede a re-renderização automatica do react
+
+3- Ele possui um ARRAY DE DEPENDÊNCIAS, que deve contem os dados que ativem a execução da função de forma automatica
+
+4- ele sempre está presente nas REQUISIÇÕES ASSINCRONAS
+*/
