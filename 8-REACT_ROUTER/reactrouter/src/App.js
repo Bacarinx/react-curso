bcb740f86d2c1e7e77fd5dd2c2aff2cfff47c931
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-04-11 17:27:13 -0300

    Carregamento individual

--- 8-REACT_ROUTER/reactrouter/src/App.js
+++ 8-REACT_ROUTER/reactrouter/src/App.js
@@ -6,6 +6,7 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 //pages
 import Home from './pages/Home'
 import About from './pages/about'
+import Product from './pages/Product'
 
 //components
 import Navbar from './components/Navbar'
@@ -20,6 +21,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          {/* 4 - Rota Dinâmica */}
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </div>
@@ -30,7 +33,6 @@ export default App;
 
 /*
                     Configurando o react router 
-
 Primeiro passo é fazer a instalação dele na pasta do projeto (npm i react-router-dom)
 
 BrowserRouter: Define onde a área do nosso app que vai trocar as páginas
@@ -40,17 +42,45 @@ Routes: Define as rotas
 Route: Um delemento deste para cada rota, configurar com path e componente da rota
 
 
-                    Adicionando os links
-  
+                    Adicionando os links 
 Para criar links entre as páginas precisamos utiliozar o LINK do react Router
 
 No link configuramos o parâmetro "to", que recebe a URL/path que será redirecionadp quem clicar no link
 
 
-                    Carregando Dados
-                    
+                    Carregando Dados                  
 Exercitando novamente o carregamento de dados com o hook useFetch    
 
 Depois poderemos utilizá-los para o "carregamento de dados individuais"
+
+
+                    Rota Dinâmica
+Para criar uma rota Dinâmica, vamos precisar definir uma nova Router em App.js
+
+Ela deve ter o padrão de : "/products/:id"
+
+onde :id é o dado dinâmico, ou seja, podemos ter qualque valor
+
+Na página podemos utilizar o hook useParams para resgatar esta informação
+
+                    Carregando Dado Individual
+Graças ao passos dados da ultima aula, será facil de realizar o carregamento individual
+
+Vamos utikizar o id recebido para FOrmar a nova URL
+
+Vamos utilizar o hook useFetch para trazer o item
+
+Por ultimo passo, faremos a validação e impressão do mesmo no JSX
+
+                    Nested Route
+As nested routes indicam URLs mais complexas como: "/products/:id/something"
+
+Neste caso vamos precisar criar um componente que corresponda com o padrão indicado e também a URL em App.js
+
+Na nested route teremos o acesso ao parâmetro da URL também
+
+
+
+
 
 */
---
+++ 8-REACT_ROUTER/reactrouter/src/pages/Product.js
@@ -0,0 +1,31 @@
+import { useParams } from "react-router-dom"
+import { useFetch } from "../hooks/useFetch"
+
+const Product = () => {
+
+    // 4 - rota Dinâmica
+    const { id } = useParams()
+
+    // 5 - Carregamneto dado individual
+    const url ="http://localhost:3000/products/" + id
+
+    const {data: product, loading, error} = useFetch(url)
+
+    console.log(product)
+
+    return (
+        <div>
+            <p>ID do produto: {id}</p>
+            {error && <p>Ocorreu um erro</p>}
+            {loading && <p>Carregando Produto</p>}
+            {product && (
+                <div>
+                    <h1>{product.name}</h1>
+                    <p>R${product.price}</p>
+                </div>
+            )}
+        </div>
+    )   
+}
+
+export default Product
