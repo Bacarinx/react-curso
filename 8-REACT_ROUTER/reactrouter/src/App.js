cfda3187ed771cc49c0e9ac4c1aaacf81a1f6bc7
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-04-11 16:26:43 -0300

    importando base reacte Router

--- 8-REACT_ROUTER/reactrouter/src/App.js
+++ 8-REACT_ROUTER/reactrouter/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+
+// 1 - config react router
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+//pages
+import Home from './pages/Home'
+import About from './pages/about'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+/*
+                    Configurando o react router 
+
+Primeiro passo é fazer a instalação dele na pasta do projeto (npm i react-router-dom)
+
+BrowserRouter: Define onde a área do nosso app que vai trocar as páginas
+
+Routes: Define as rotas
+
+Route: Um delemento deste para cada rota, configurar com path e componente da rota
+
+
+                    Adicionando os links
+  
+Para criar links entre as páginas precisamos utiliozar o LINK do react Router
+
+No link configuramos o parâmetro "to", que recebe a URL/path que será redirecionadp quem clicar no link
+
+
+*/
