5404de935f8b988c89f7609ffcc5f3231636ff4d
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-04-12 15:49:00 -0300

    Provider

--- 9_CONTEXT_API/contextapi/src/App.js
+++ 9_CONTEXT_API/contextapi/src/App.js
@@ -31,3 +31,26 @@ function App() {
 }
 
 export default App;
+
+/* 
+            Context
+-Primeiro vamos ter que criar o context
+
+-Sempre deve ter a primeira letra maiuscula, e geralmente termina em Context: ex--> SomeCOntext.js
+
+-Deixamos a pasta context em stc
+
+-Onde formos utilizar o valor do context, o arquivo precisa ser importado
+
+            Provider
+-Ele vai delimitar onde o contexto é utilizado
+
+-Vamos criar uma espécie de componente com a prop Children
+
+-O provider deve encapsilar os demais componentes em que precisamos consultar ou alterar o valor;
+
+-FIca em App.js ou index.js
+
+-Poderemos compartilhar o valor do contexto em todos os componentes
+
+*/
