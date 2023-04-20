49156fcca573fe3226bd9cc9dc2a7b98f0f54c60
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-04-20 10:32:02 -0300

    requisição de cadastro

--- 10_miniblog/miniblog/src/firebase/config.js
+++ 10_miniblog/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFireStore } from "firebase/firestore"
+import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
   apiKey: "AIzaSyCl2OFL7jo14FYr8gYM9LxywDlyKgq8QUo",
@@ -12,6 +12,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-const db = getFireStore(app);
+const db = getFirestore(app);
 
 export { db }; 
